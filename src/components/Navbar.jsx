d0a87c96e40ea9287ca6fdfa63ea118e38dc3a34
Alexrldev <118137925+Alexrldev@users.noreply.github.com>
2024-12-18 00:22:52 -0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,16 +28,16 @@ const Navbar = () => {
                 </ul>
             </nav>
             : 
-            <nav className=" flex justify-between m-2 mt-0 bg-gray-800 p-2 rounded-lg rounded-t-none">
+            <nav className=" flex m-2 mt-0 bg-gray-800 p-2 rounded-lg rounded-t-none justify-center">
                 <ul className="flex gap-2">
                     <li>
-                        <Link to="html/trivia/login" className="p-1 rounded-lg mx-4 hover:bg-slate-700 active:bg-black">Iniciar sesión</Link>
+                        <Link to="html/trivia/login" className="rounded-lg mx-1 hover:bg-slate-700 active:bg-black sm:p-2 max-[530px]:text-sm max-[450px]:text-[13px] max-[385px]:text-[11px] max-[336px]:text-[10px]">Iniciar sesión</Link>
                     </li>
                     <li>
-                        <Link to="https://github.com/Alexrldev/backend_trivia" className="mx-4 p-1 rounded-lg hover:bg-slate-700 active:bg-black" >Repositorio backend</Link>
+                        <Link to="https://github.com/Alexrldev/backend_trivia" className="mx-1 rounded-lg hover:bg-slate-700 active:bg-black sm:p-2 max-[530px]:text-sm max-[450px]:text-[13px] max-[385px]:text-[11px] max-[336px]:text-[10px]" >Repositorio backend</Link>
                     </li>
                     <li>
-                        <Link to="https://github.com/Alexrldev/trivia_frontend" className="mx-4 p-1 rounded-lg hover:bg-slate-700 active:bg-black" >Repositorio Frontend</Link>
+                        <Link to="https://github.com/Alexrldev/trivia_frontend" className="mx-1 rounded-lg hover:bg-slate-700 active:bg-black sm:p-2 max-[530px]:text-sm max-[450px]:text-[13px] max-[385px]:text-[11px] max-[336px]:text-[10px]">Repositorio Frontend</Link>
                     </li>
                 </ul>
             </nav>
